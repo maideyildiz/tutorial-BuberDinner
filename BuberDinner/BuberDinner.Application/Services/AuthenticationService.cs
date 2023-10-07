8155869f4d6cd0f40b942fec88340c2ba7e93166
maidey <maideyildizz@gmail.com>
2023-10-07 12:24:35 +0300

    jwttoken implementation

--- BuberDinner/BuberDinner.Application/Services/AuthenticationService.cs
+++ BuberDinner/BuberDinner.Application/Services/AuthenticationService.cs
@@ -1,14 +1,24 @@
+using BuberDinner.Application.Common.Interfaces.Authentication;
+
 namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
     public AuthenticationResult Register(string firsName, string lastName, string email, string password)
     {
-        return new AuthenticationResult(Guid.NewGuid(), firsName, lastName, email, "token");
+        Guid userId = Guid.NewGuid();
+        var token = _jwtTokenGenerator.GenerateToken(userId, firsName, lastName);
+        return new AuthenticationResult(userId, firsName, lastName, email, token);
     }
 
     public AuthenticationResult Login(string email, string password)
     {
+        //var token = _jwtTokenGenerator.GenerateToken(Guid.NewGuid(), firsName, lastName);
         return new AuthenticationResult(Guid.NewGuid(), "firsName", LastName: "lastName", email, "token");
     }
 }
