3510e981467668efae0fcc4e02f64040121a24a9
maidey <maideyildizz@gmail.com>
2023-10-09 22:37:56 +0300

    CQRS MediatR implementation

--- BuberDinner/BuberDinner.API/Controllers/AuthenticationController.cs
+++ BuberDinner/BuberDinner.API/Controllers/AuthenticationController.cs
@@ -1,7 +1,10 @@
-using BuberDinner.Application.Services.Authentication;
+using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Application.Authentication.Queries.Login;
 using BuberDinner.Contracts.Authentication;
 using BuberDinner.Domain.Common.Errors;
 using ErrorOr;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberDinner.API.Controllers;
@@ -9,19 +12,22 @@ namespace BuberDinner.API.Controllers;
 [Route("auth")]
 public class AuthenticationController : ApiController
 {
-    private readonly IAuthenticationService _authenticationService;
-    public AuthenticationController(IAuthenticationService authenticationService)
+    private readonly ISender _mediator;
+    public AuthenticationController(
+        ISender mediator)
     {
-        _authenticationService = authenticationService;
+        _mediator = mediator;
     }
     [HttpPost("register")]
-    public IActionResult Register(RegisterRequest request)
+    public async Task<IActionResult> Register(RegisterRequest request)
     {
-        var authResult = _authenticationService.Register(
+        var command = new RegisterCommand(
             request.FirstName,
-            request.LastName
-        , request.Email,
-        request.Password);
+            request.LastName,
+            request.Email,
+            request.Password
+        );
+        ErrorOr<AuthenticationResult> authResult = await _mediator.Send(command);
 
         return authResult.Match(
             authResult => Ok(MapAuthResult(authResult)),
@@ -31,10 +37,10 @@ public class AuthenticationController : ApiController
 
 
     [HttpPost("login")]
-    public IActionResult Login(LoginRequest request)
+    public async Task<IActionResult> Login(LoginRequest request)
     {
-        var authResult = _authenticationService.Login(request.Email,
-        request.Password);
+        var query = new LoginQuery(request.Email, request.Password);
+        var authResult = await _mediator.Send(query);
 
         if (authResult.IsError && authResult.FirstError == Errors.Authentication.InvalidCredentials)
         {
--- BuberDinner/BuberDinner.Application/DependencyInjection.cs
+++ BuberDinner/BuberDinner.Application/DependencyInjection.cs
@@ -1,4 +1,4 @@
-using BuberDinner.Application.Services.Authentication;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BuberDinner.Application;
@@ -6,7 +6,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddScoped<IAuthenticationService, AuthenticationService>();
+        services.AddMediatR(cfg => { cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly); });
         return services;
     }
 }
